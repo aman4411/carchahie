70b906f547c7400ecf226145f249d44f13125f03
Aman Mehta <amanmehta4411@gmail.com>
2023-07-16 15:14:11 +0530

    db config change

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,10 +1,15 @@
 <?php
 function getDBConnection()
 {
-    $dbhost = "remotemysql.com";
-    $dbuser = "cU6G9jfUxz";
-    $dbpass = "7UsmOA93Kf";
-    $db = "cU6G9jfUxz";
+    // $dbhost = "remotemysql.com";
+    // $dbuser = "cU6G9jfUxz";
+    // $dbpass = "7UsmOA93Kf";
+    // $db = "cU6G9jfUxz";
+
+    $dbhost = "db4free.net";
+    $dbuser = "root_aman";
+    $dbpass = "P@ssw0rd";
+    $db = "codeigniter";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
     return $conn;
 }
